3790e71ab9bb804a482106f9a8d4b718c0b50ede
hawlermathew2007 <hawlermathewgit@gmail.com>
2025-07-12 23:28:01 +0800

    Add SETNAME and WHOIS (for server side)

--- tcp_client.py
+++ tcp_client.py
@@ -4,36 +4,53 @@ import sys
 
 client_name = "*"
 
-TARGET_HOST = "<HOST_IP>"
+TARGET_HOST = "172.20.10.2"
 TARGET_PORT = 9998
 SENTINEL = 'QUIT<!>'
 
 CMD_PROMPT_TXT = f"[{client_name}]>> "
 MSG_PROMPT_TXT = "[CHAT WITH ALL]>> "
+current_prompt = CMD_PROMPT_TXT
 # MSG_PROMPT_TXT = "[CHAT WITH <NAME/IP>]>> "
+
+TOKEN_SETNAME = "3ccaf424d754a5a65337f4b742905b1e" # setnameisneeded#0000
+TOKEN_DM = "66d29e9c3fd4ff07a8046daa3fa37bcd" # directmessageisneeded#0001
+
+# currently there is a vulnerability rn... Is that not only the server can set your name by using its token
+# but the other fellow who is DM-ing with u can too :v
+# they just need to type: <token>:// <your_funny_name>
+# and you or even me already get cooked lol
+# the reason for this is because how your message is set to travel
+# from YOU -> SERVER (specifically the listen_client func) -> YA_FUNNY_FELLOW
 
 INSTRUCTION = """========== COMMAND LINE ==========
 1. Direct Message to one person := DM <CLIENT_IP>:<PORT>/<NAME>
-2. Send Message to everyone := BC
+2. Send Message to everyone := CHAT
 3. Set your name := SETNAME <YOUR_NAME>
 4. Back to chat := BACK<!>
 5. Exit server := QUIT<!>
-==================================
-*Note: For 1, 2, 4 to execute, must switch to command mode [*]>> using 3: BACK<!>"""
+*Note: For 1, 2, 4 to execute, must switch to command mode [*]>> using 3: BACK<!>
+=================================="""
 
 # create TCP client
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket.AF_INET >> use a standard IPv4 address or hostname | socket.SOCK_STREAM >> TCP
 
 def listen_response():
     global client
+    global client_name
 
     try:
         while True:
             response = client.recv(1024)
             if response:
+                server_msg = response.decode()
                 print(35*"-")
-                print(f"[{TARGET_HOST}:{TARGET_PORT}]: {response.decode()}")
-                sys.stdout.write("[*]>> ")
+                if TOKEN_SETNAME in server_msg and server_msg.split()[1] != TOKEN_SETNAME:
+                    client_name = server_msg.split()[1]
+                    print("[*] Your name is set successfully.")
+                else:
+                    print(f"[{TARGET_HOST}:{TARGET_PORT}]: {server_msg}")
+                sys.stdout.write(current_prompt)
                 sys.stdout.flush()
     except ConnectionAbortedError:
         print("Disconnected...")
@@ -47,27 +64,51 @@ def listen_response():
 def direct_msg():
     pass
 
-def boardcast_msg(client, msg):
+def send_msg_to_all(client, msg):
     client.send(msg)    
 
+def set_name(client, name): # potentially a thread
+    client.send(f"{TOKEN_SETNAME}//: {name}".encode("utf-8"))
+
+def request_dm(client, fellow):
+    client.send(f"")
 
 try:
     client.connect((TARGET_HOST, TARGET_PORT))
-
-    msg = input(CMD_PROMPT_TXT).replace('\n', '')
+    
+    print(INSTRUCTION + "\n")
+    display_prompt = CMD_PROMPT_TXT
 
     threading.Thread(target=listen_response).start()
 
-    print(INSTRUCTION)
-
-    while msg != SENTINEL:
-        boardcast_msg(client, msg.encode('utf-8'))
-        msg = input(CMD_PROMPT_TXT).replace('\n', '')
+    while True:
+        prompt = input(display_prompt).replace('\n', '')
+        if display_prompt == CMD_PROMPT_TXT:
+            if prompt.upper() == "CHAT":
+                display_prompt = MSG_PROMPT_TXT
+                current_prompt = display_prompt
+            elif "DM" in prompt.upper() and ":" in prompt.upper(): # the ":" may need to be deleted
+                direct_msg()
+            elif "SETNAME" in prompt.upper():
+                if len(prompt.split()) == 2 and prompt.split()[0].upper() == "SETNAME":
+                    set_name(client, prompt.split()[1])
+                else:
+                    print("[*] Invalid input. Ex: SETNAME Michael_Jackson90s")
+            elif prompt.upper() == SENTINEL:
+                break
+            else:
+                print("[*] Invalid input.")
+        elif prompt.upper() == "BACK<!>" and display_prompt != CMD_PROMPT_TXT:
+            display_prompt = CMD_PROMPT_TXT
+            current_prompt = display_prompt
+        elif display_prompt == MSG_PROMPT_TXT:
+            send_msg_to_all(client, prompt.encode('utf-8'))
 
 except ConnectionRefusedError:
     print("[*] Host is not on...")
 except Exception as e:
     print("[*] An error occurs :(")
+    print(e)
 finally:
     client.close()
     sys.exit()
--- tcp_server.py
+++ tcp_server.py
@@ -6,15 +6,20 @@ import time
 IP = '0.0.0.0'
 PORT = 9998
 FORMAT = 'utf-8'
-SERVER_NAME = "*"
+server_name = "*"
 
 clients = set()
 clients_lock = threading.Lock()
 
 registered_name = {} # name must be unique
+ban_lists = []
 
-CMD_PROMPT_TXT = f"[{SERVER_NAME}]>> "
+TOKEN_SETNAME = "3ccaf424d754a5a65337f4b742905b1e" # setnameisneeded#0000
+TOKEN_DM = "66d29e9c3fd4ff07a8046daa3fa37bcd" # directmessageisneeded#0001
+
+CMD_PROMPT_TXT = f"[{server_name}]>> "
 BOARDCAST_PROMPT_TXT = "[*] WRITE A MSG TO EVERYONE>> "
+current_prompt = CMD_PROMPT_TXT
 
 INSTRUCTION = """========== COMMAND LINE ==========
 1. Direct Message to client := DM <CLIENT_IP>:<PORT>/<NAME>
@@ -23,23 +28,26 @@ INSTRUCTION = """========== COMMAND LINE ==========
 4. Check client's IP and PORT through NAME := WHOIS <NAME>
 5. Switch back to command mode for command line := BACK<!>
 6. Terminate server := QUIT<!>
-==================================
-*Note: For 1, 2, 4 to execute, must switch to command mode [*]>> using 3: BACK<!>"""
+Note:
+- For 1, 2, 4 to execute, must switch to command mode [*]>> using 3: BACK<!>
+- The name that you set can only see by you unfortunately...
+=================================="""
 
 # Variable for if_server_prompt_need_esc
 goback_offset = 0
 interruptions = [] # same as client_ids i guess # get the first element
-current_prompt = CMD_PROMPT_TXT
 
 def main(): # DM need to check if the provided client ID exist
     global current_prompt
+    global CMD_PROMPT_TXT
 
-    print(INSTRUCTION)
+    print(INSTRUCTION + "\n")
 
     try:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((IP, PORT))
         server.listen(5)
+
         print(f'[*] Listening on {IP}:{PORT}')
 
         threading.Thread(target=capture_client, args=(server, ), daemon=True).start() # Start capturing connection request
@@ -47,41 +55,53 @@ def main(): # DM need to check if the provided client ID exist
 
         time.sleep(1)
 
-        display_prompt = CMD_PROMPT_TXT
         dm_client = "" # remember to empty client
         dm_client_txt = "" # remember to empty
 
         while True:
-            # server can only send lowercase msg :V
-            prompt = input(display_prompt).upper()
+            display_prompt = CMD_PROMPT_TXT
+            prompt = input(display_prompt)
             if display_prompt == CMD_PROMPT_TXT:
                 # actually still need to empty sth bruh
-                if "DM" in prompt and ":" in prompt:
+                if "DM" in prompt.upper() and ":" in prompt.upper(): # the ":" will be deleted if add name
                     is_targetted_client_exists = False
-                    targetted_client_id = prompt.split()[1]
+                    targetted_client_id = prompt.split()[1] # must check if len(prompt.split()) == 2
                     if len(clients) > 0:
-                        for client in clients:
-                            test_client_id = f"{client.getpeername()[0]}:{client.getpeername()[1]}"
-                            if test_client_id == targetted_client_id:
-                                dm_client = client
-                                is_targetted_client_exists = True
-                        if is_targetted_client_exists:
-                            dm_client_txt = f"[*] WRITE A MSG TO {targetted_client_id}>> "
-                            display_prompt = dm_client_txt
-                            current_prompt = display_prompt
+                        if len(prompt.split()) == 2:
+                            for client in clients:
+                                test_client_id = f"{client.getpeername()[0]}:{client.getpeername()[1]}"
+                                if test_client_id == targetted_client_id:
+                                    dm_client = client
+                                    is_targetted_client_exists = True
+                            if is_targetted_client_exists:
+                                dm_client_txt = f"[*] WRITE A MSG TO {targetted_client_id}>> "
+                                display_prompt = dm_client_txt
+                                current_prompt = display_prompt
+                            else:
+                                print("[*] No client with that identity...")
                         else:
-                            print("[*] No client with that identity...")
+                            print("[*] Please provide one argument only. Ex: DM 192.168.0.1:53689.")
                     else:
-                        print("[*] You currently have no client...")
-                elif prompt == "BC":
+                        print("[*] You  have no client...")
+                elif prompt.upper() == "BC":
                     display_prompt = BOARDCAST_PROMPT_TXT
                     current_prompt = display_prompt
-                elif prompt == "QUIT<!>":
+                elif "SETNAME" in prompt.upper():
+                    if len(prompt.split()) == 2 and prompt.split()[0].upper() == "SETNAME":
+                        set_server_name(prompt.split()[1])
+                    else:
+                        print("[*] Invalid input. Ex: SETNAME evil_AdMin#55")
+                elif "WHOIS" in prompt.upper():
+                    if len(prompt.split()) == 2 and prompt.split()[0].upper() == "WHOIS":
+                        who_is(prompt.split()[1])
+                    else:
+                        print("[*] Invalid input. Ex: WHOIS poor_victim@LOL")
+                elif prompt.upper() == "QUIT<!>":
                     break
                     # terminate all clients and threads and server
                 else:
                     print("[*] Invalid input.")
-            elif prompt == "BACK<!>" and display_prompt != CMD_PROMPT_TXT:
+            elif prompt.upper() == "BACK<!>" and display_prompt != CMD_PROMPT_TXT:
                 dm_client = ""
                 display_prompt = CMD_PROMPT_TXT
                 current_prompt = display_prompt
@@ -89,7 +109,7 @@ def main(): # DM need to check if the provided client ID exist
                 if len(clients) > 0:
                     boardcast_msg(prompt)
                 else:
-                    print("[*] You currently have no client...")
+                    print("[*] You have no client...")
             elif display_prompt == dm_client_txt:
                 direct_msg(dm_client, prompt)
 
@@ -100,7 +120,6 @@ def main(): # DM need to check if the provided client ID exist
         sys.exit()
 
 
-
 def capture_client(server):
     while True:
         client, address = server.accept()
@@ -108,34 +127,80 @@ def capture_client(server):
         threading.Thread(target=listen_client, args=(client, )).start()
         put_in_interruptions(f"[ACTIVE CLIENTS]: {get_active_clients()}")
 
+
 def boardcast_msg(msg):   
     with clients_lock:
         for client in clients:
-            client.send(msg.lower().encode(FORMAT))
+            client.send(msg.encode(FORMAT))
 
 
 def direct_msg(client, msg):
     client.send(msg.lower().encode(FORMAT))
 
-def terminate_msg():
+
+def delete_client():
     pass
 
+
+def set_server_name(name):
+    global server_name
+    global CMD_PROMPT_TXT
+
+    server_name = name
+    CMD_PROMPT_TXT = f"[{server_name}]>> "
+    print(f"[*] Successfully changed your name to \"{name}\"")
+
+
+def set_client_name(client, name):
+    # also check if the name is used
+    if name not in registered_name.keys():
+        client_info = client.getpeername()
+        client_id = f"{client_info[0]}:{client_info[1]}"
+        registered_name.update({name : client_id})
+        client.send(f"{TOKEN_SETNAME}//: {name}".encode(FORMAT))
+        put_in_interruptions(f"[*] {client_id} ---> {name}")
+    else:
+        decline_msg = "Sorry, your name has already been taken..."
+        client.send(decline_msg.encode(FORMAT))
+
+
+def who_is(name):
+    try:
+        if len(registered_name) > 0:
+            client_id = registered_name[name]
+            print(f"[*] {name} ---> {client_id}")
+        else:
+            print("[*] You have no clients...")
+    except KeyError:
+        print("[*] Unknown...")
+    except Exception as e:
+        print(f"[*] Error: {e}")
+
+
 def listen_client(client_socket):
-    connected = True
+    global registered_name
+
     client_info = client_socket.getpeername()
-    client_id = f"{client_info[0]}:{client_info[1]}"
+
+    connected = True
     with clients_lock:
         clients.add(client_socket)
     try:
         while connected:
             request = client_socket.recv(1024)
             if request:
+                client_id = f"{client_info[0]}:{client_info[1]}"
+                if client_id in registered_name.values():
+                    client_id = [registered_name for registered_name, client in registered_name.items() if client == client_id][0]
                 request_msg = request.decode("utf-8")
                 client_msg = f'[{client_id}]: {request_msg}'
-                put_in_interruptions(client_msg)
+                if TOKEN_SETNAME in request_msg and request_msg.split()[0][:-3] == TOKEN_SETNAME and request_msg.split()[1] != TOKEN_SETNAME:
+                    set_client_name(client_socket, request_msg.split()[1])
+                else:
+                    put_in_interruptions(client_msg)
 
     except ConnectionResetError:
-        put_in_interruptions(f"Client {client_id} disconnected...")
+        put_in_interruptions(f"Client {client_info[0]}:{client_info[1]} disconnected...")
     except Exception as _:
         put_in_interruptions("Server failed...")
         sys.exit()
@@ -188,10 +253,9 @@ if __name__ == '__main__':
 
 # I know yall gonna ask why no class, but stfu bruh
 
-# use: netstat -ano|findstr <PORT>
+# use: netstat -ano | findstr <PORT>
 # to check if the server is actually listening
 
-# add name => setname() + whois()
-# delete client + block := DEL <CLIENT> (--block)
+# delete client + block := BAN <CLIENT> (--forerver)
 
-# add pwn >:) => able to extract user data + send funny message
+# add pwn >:) => able to extract user data + send funny message box
